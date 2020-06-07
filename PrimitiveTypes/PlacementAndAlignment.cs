9391148af7b9a9a39e671e1a85315a3031379299
Mikhail Motlokhov <m.motlokhov@yandex.ru>
2020-06-07 22:31:56 +0300

    Добавил подглавы ссылочные, значимые типы и размещение полей

---
+++ PrimitiveTypes/PlacementAndAlignment.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveTypes
+{
+    /// <summary>
+    /// Размещений и выравнивание полей
+    /// </summary>
+    class PlacementAndAlignment
+    {
+        /// <summary>
+        /// По умолчанию для классов значение LayoutKind.Auto
+        /// </summary>
+        [StructLayout(LayoutKind.Auto)]
+        class T_Class
+        {
+            int m_int;
+            byte m_byte;
+        }
+
+        /// <summary>
+        /// По умолчанию для структур значение LayoutKind.Sequential
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        struct T_Struct
+        {
+            int m_int;
+            byte m_byte;
+        }
+
+        /// <summary>
+        /// Значение LayoutKind.Explicit позволяет явно задавать смещение полей с помощью FielsOffset
+        /// </summary>
+        [StructLayout(LayoutKind.Explicit)]
+        struct ExplicitStruct
+        {
+            [FieldOffset(0)]
+            byte m_byte;
+            [FieldOffset(1)]
+            int m_int;
+        }
+
+    }
+}
---
+++ PrimitiveTypes/ValueTypes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveTypes
+{
+    /// <summary>
+    /// Структура это значимый тип данных
+    /// </summary>
+    struct ValueTypes
+    {
+        private int m_int;
+        private List<int> m_list;
+        /*
+          1) Память для значимых типов не входящих в состав классов выделяется в стеке потока
+          2) При инициализации поля заполняются значением  по умолчанию
+          3) По умолчанию запечатан(sealed)
+          4) Структуры не могут иметь авто инициализаторы
+         */
+
+        /// <summary>
+        /// Присвоение значения
+        /// </summary>
+        private void ValueAssignmetn()
+        {
+            //При присвоении экземпляра объекта значимого типа другому экземпляру происходит копирование значений полей
+            var c1 = new ValueTypes();
+            c1.m_int = 1;
+            c1.m_list = new List<int>() { 1 };
+
+            var c2 = c1;
+            c2.m_int = 2;
+
+            var equal = c2.m_int == c1.m_int; //false
+            Console.WriteLine(equal);
+            //значение m_int измениется в только в объекте c2;
+        }
+
+    }
+}
