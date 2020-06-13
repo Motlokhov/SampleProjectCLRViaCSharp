05eb483f95d3684e18a867053dc6e1772b901bc9
Mikhail Motlokhov <m.motlokhov@yandex.ru>
2020-06-13 14:14:07 +0300

    Описание упаковки объектов

--- PrimitiveTypes/BoxingAndUnboxing.cs
+++ PrimitiveTypes/BoxingAndUnboxing.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace PrimitiveTypes
 {
     class BoxingAndUnboxing
     {
+
+        /// <summary>
+        /// 1) В управляемой куче выделяется память. Ее объем определяется длиной значимого типа + (ссылка на тип и индекс блока синхронизации)
+        /// 2) Поля значимого типа копируются в память выделеную в куче.
+        /// 3) Возвращается адрес объекта.
+        /// </summary>
+        private void BoxingPoint()
+        {
+            ArrayList array = new ArrayList();
+            var p = new Point(); // выделение памяти для point в стеке
+            for (var i = 0; i < 10; i++)
+            {
+                p.X = p.Y = i; //инициализация членов в значимом типе
+                array.Add(p); // упаковка значимого типа идобавление ссылки в ArrayList
+            }
+        }
     }
 }
