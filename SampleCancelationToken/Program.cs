3644945c5c770977b1175680e01c0af67210051e
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-02-21 17:27:35 +0300

    Add cancelationToken samples

---
+++ SampleCancelationToken/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleCancelationToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            cts.Token.Register(() => Console.WriteLine("At {0}", DateTime.Now)); // second invoke after cancel
+            cts.Token.Register(() => Console.Write("Count is canceled. ")); //First invoke after cancel
+
+            ThreadPool.QueueUserWorkItem(_ => Count(cts.Token, 10));
+            Console.WriteLine("Tap to stop");
+            Console.ReadKey();
+            cts.Cancel();
+            Console.WriteLine("Tap to exit");
+            Console.ReadKey();
+        }
+
+        private static void Count(CancellationToken token, int countTo)
+        {
+            for(int i = 0; i < countTo; i++)
+            {
+                if(token.IsCancellationRequested)
+                    break;
+
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("done");
+        }
+    }
+}
---
+++ SampleLinkedCancelationToken/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleLinkedCancelationToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts1 = new CancellationTokenSource();
+            cts1.Token.Register(() => Console.WriteLine("cts1 canceled"));
+
+            var cts2 = new CancellationTokenSource();
+            cts2.Token.Register(() => Console.WriteLine("cts2 canceled"));
+            cts2.CancelAfter(TimeSpan.FromSeconds(3));
+
+            var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(cts1.Token, cts2.Token);
+            linkedCts.Token.Register(() => Console.WriteLine("linked cts caceled"));
+            Console.ReadLine();
+        }
+    }
+}
